5302dcb34e5b308aa91ab0b748ec98a3647318c5
nighthunter1305 <mthanhdat1305@gmail.com>
2024-10-27 21:21:32 +0700

    Update App.js

--- frontend/src/pages/AdminPage/App.js
+++ frontend/src/pages/AdminPage/App.js
@@ -5,6 +5,7 @@ import Sidebar from './components/Sidebar';
 import Navbar from './components/Navbar';
 import Dashboard from './pages/Dashboard';
 import Users from './pages/Users';
+import UserDetails from './pages/UserDetails';
 import Transactions from './pages/Transactions';
 import Employees from './pages/Employees';
 import AddEmployee from './pages/AddEmployee';
@@ -30,6 +31,7 @@ function App() {
                         <Routes>
                             <Route path="/" element={<Dashboard />} />
                             <Route path="/users" element={<Users />} />
+                            <Route path="/users/:id" element={<UserDetails />} />
                             <Route path="/transactions" element={<Transactions />} />
                             <Route path="/employees" element={<Employees employees={employees} />} />
                             <Route path="/add-employee" element={<AddEmployee addEmployee={addEmployee} />} />
